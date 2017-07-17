c31cd374a9d9a39811910cfd32e05677791c1cdd
yueweiyang <yueweiyang92924@gmail.com>
2017-07-17 18:51:24 -0400

    code Fashionshow

---
+++ FashionShow/fashionShow.cpp
@@ -0,0 +1,140 @@
+//
+//  main.cpp
+//  ModelScore
+//
+//  Created by Yue wei Yang on 7/14/17.
+//  Copyright © 2017 Yue wei Yang. All rights reserved.
+//
+
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char * argv[]){
+  ifstream input;
+  input.open(argv[1],ifstream::in);
+
+  vector<string> lines;
+  string s;
+  while (getline(input,s)){
+    lines.push_back(s);
+  }
+
+  input.close();
+
+  ofstream output;
+  output.open("output.txt");
+ 
+  size_t nline = 1;
+  size_t nCase = 0;
+  while (nline<lines.size()){
+    char* pEnd;
+    long int N = strtol(lines[nline].c_str(),&pEnd,10);
+    long int M = strtol(pEnd,&pEnd,10);
+    int * crossModel = new int[N*N]();
+    int * plusModel = new int[N*N]();
+    int * Row = new int[N]();
+    int * Col =new int[N]();
+    int * DiagonalSub = new int[N]();
+    int * DiagonalSum = new int[2*N-1]();
+    int points = 0;
+    for (long int n = 1; n<=M; n++){
+      char model = lines[nline+n].c_str()[0];
+      pEnd = &lines[nline+n][2];
+      long int row = strtol(pEnd,&pEnd,10);
+      long int col = strtol(pEnd,&pEnd,10);
+      switch (model){
+      case '+':
+	plusModel[(row-1)*N+col-1]=1;
+	DiagonalSub[abs(row-col)]=1;
+	DiagonalSum[row+col-2]=1;
+	points++;
+	break;
+      case 'x':
+	crossModel[(row-1)*N+col-1]=1;
+	Row[row-1]=1;
+	Col[col-1]=1;
+	points++;
+	break;
+      case 'o':
+	plusModel[(row-1)*N+col-1]=1;
+	crossModel[(row-1)*N+col-1]=1;
+	DiagonalSub[abs(row-col)]=1;
+	DiagonalSum[row+col-2]=1;
+	Row[row-1]=1;
+	Col[col-1]=1;
+	points +=2;
+	break;
+      }
+    }
+    nline +=M+1;
+    nCase++;
+
+    output<<"Case #"<<nCase<<": ";
+
+    size_t cModel = 0;
+    for (long int nRow=0;nRow<N;nRow++){
+      if (Row[nRow]==1){
+	continue;
+      }
+      for(long int nCol=0;nCol<N&&Row[nRow]!=1;nCol++){
+	if (Col[nCol]==1){
+	  continue;
+	}
+	else {
+	  crossModel[nRow*N+nCol]=2;
+	  Row[nRow]=1;
+	  Col[nCol]=1;
+	  points++;
+	  cModel++;
+	}
+      }
+    }
+
+    for (long int nRow=0;nRow<N;nRow++){
+      for (long int nCol=0;nCol<N;nCol++){
+	if (DiagonalSub[abs(nRow-nCol)]==1||DiagonalSum[nRow+nCol]==1){ continue;
+	}
+	else {
+	  plusModel[nRow*N+nCol]=2;
+	  DiagonalSub[abs(nRow-nCol)]=1;
+	  DiagonalSum[nRow+nCol]=1;
+	  points++;
+	  cModel++;
+	}
+
+	if (plusModel[nRow*N+nCol]*crossModel[nRow*N+nCol]==4){
+	  cModel--;
+	}
+      }
+    }
+
+    output<<points<<" "<<cModel<<endl;
+
+    for (long int nRow=0;nRow<N;nRow++){
+      for (long int nCol=0;nCol<N;nCol++){
+	if (plusModel[nRow*N+nCol]*crossModel[nRow*N+nCol]>1){
+	  output<<"o "<<nRow+1<<" "<<nCol+1<<endl;
+	}
+	else if (plusModel[nRow*N+nCol]==2){
+	  output<<"+ "<<nRow+1<<" "<<nCol+1<<endl;
+	}
+	else if (crossModel[nRow*N+nCol]==2){
+	  output<<"x "<<nRow+1<<" "<<nCol+1<<endl;
+	}
+      }
+    }
+    delete crossModel;
+    delete plusModel;
+    delete Row;
+    delete Col;
+    delete DiagonalSub;
+    delete DiagonalSum;
+  }
+
+  return EXIT_SUCCESS;
+}
